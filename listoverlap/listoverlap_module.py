a40843c8215fd9b64440560863b2ac35f35e2b66
Tomi <tomi@192.168.1.2>
2016-05-22 21:29:51 +0200

    List Overlap kata

---
+++ listoverlap/listoverlap_module.py
@@ -0,0 +1,10 @@
+def listoverlap(list1, list2):
+    return
+
+
+def main():
+    return
+
+
+if __name__ == '__main__':
+    main()
---
+++ listoverlap/test.py
@@ -0,0 +1,21 @@
+import unittest
+from listoverlap_module import listoverlap
+
+
+# Here's our "unit tests".
+
+
+class ListOverlapTestCase(unittest.TestCase):
+    def test_listoverlap(self):
+        a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+        b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+        c = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 21, 34, 55, 89]
+        self.assertEqual(listoverlap(a, b), c)
+
+
+def main():
+    unittest.main()
+
+
+if __name__ == '__main__':
+    main()
