90e3c845582647034face92d9a1f4cc846b27204
Csyk <balipaulcsik@gmail.com>
2016-05-23 14:36:13 +0200

    Before test

--- years/years_module.py
+++ years/years_module.py
@@ -1,11 +1,20 @@
 import datetime
 
 
-def years(age):
+def years(age, name, copy):
+    today = datetime.date.today()
+    time = 100 - age
+    print(
+        (name + ", you will reach 100 at " +
+         str(today.year + time) + ".\n") * copy)
     return
 
 
 def main():
+    name = input("Type your name!")
+    age = int(input("How old are you?"))
+    copy = int(input("How many copies do you want?"))
+    years(age, name, copy)
     return
 
 
