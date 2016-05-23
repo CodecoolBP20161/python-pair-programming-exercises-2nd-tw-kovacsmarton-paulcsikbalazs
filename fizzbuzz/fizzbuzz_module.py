5c289faaa224854a722626089c1726729e34c01a
Csyk <balipaulcsik@gmail.com>
2016-05-23 13:21:48 +0200

    It works

--- fizzbuzz/fizzbuzz_module.py
+++ fizzbuzz/fizzbuzz_module.py
@@ -1,9 +1,21 @@
 def fizzbuzz(number):
-    return
+    if number % 15 == 0:
+        number = 'FizzBuzz'
+        print(number)
+    elif number % 3 == 0:
+        number = 'Fizz'
+        print(number)
+    elif number % 5 == 0:
+        number = 'Buzz'
+        print (number)
+    else:
+        print(number)
+    return(number)
 
 
 def main():
-    return
+    for number in range(1, 101):
+        fizzbuzz(number)
 
 if __name__ == '__main__':
     main()
