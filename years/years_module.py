86c1bc25f4028b9677036f4e18bd47614c7b906b
Csyk <balipaulcsik@gmail.com>
2016-05-24 09:21:41 +0200

    test run, programme bug

--- years/years_module.py
+++ years/years_module.py
@@ -1,21 +1,17 @@
 import datetime
 
 
-def years(age, name, copy):
+def years(age):
     today = datetime.date.today()
-    time = 100 - age
-    print(
-        (name + ", you will reach 100 at " +
-         str(today.year + time) + ".\n") * copy)
-    return
+    return 100 - age + today.year
 
 
 def main():
-    name = input("Type your name!")
-    age = int(input("How old are you?"))
-    copy = int(input("How many copies do you want?"))
-    years(age, name, copy)
-    return
+    name = input("Type your name! ")
+    age = int(input("How old are you? "))
+    copy = int(input("How many copies do you want? "))
+    print((name + ", you will reach 100 at " + years(age) + ".\n") * copy)
+    years(name, copy, age)
 
 
 if __name__ == '__main__':
