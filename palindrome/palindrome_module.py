c78f0afa4f5945a5cb48026005aad70d7105361b
Csyk <balipaulcsik@gmail.com>
2016-05-27 16:19:49 +0200

    ready

--- palindrome/palindrome_module.py
+++ palindrome/palindrome_module.py
@@ -1,9 +1,17 @@
-def palindrome(str):
-    return
+def palindrome(string):
+    fine_word = string.replace(" ", "")
+    lower = fine_word.lower()
+    if lower == lower[::-1]:
+        print("This is a 'palindrome'!")
+        return True
+    else:
+        print("This is not a 'palindrome'!")
+        return False
 
 
 def main():
-    return
+    word = input("Please type your 'palindrome!'")
+    palindrome(word)
 
 
 if __name__ == '__main__':
