8712f8b4d899a96bda08f426cd841de999841a82
Csyk <balipaulcsik@gmail.com>
2016-05-27 16:15:59 +0200

    it works, and teseted

--- passwordgen/passwordgen_module.py
+++ passwordgen/passwordgen_module.py
@@ -1,8 +1,20 @@
+import string
+import random
+
+
 def passwordgen():
-    return
+    lowercase = (list(string.ascii_lowercase))
+    uppercase = (list(string.ascii_uppercase))
+    digits = (list(string.digits))
+    punctuation = ('!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '?')
+    charlist = ((random.sample(lowercase, 2)) + (random.sample(uppercase, 2)) +
+                (random.sample(digits, 2)) + (random.sample(punctuation, 2)))
+    password = ''.join(charlist)
+    return password
 
 
 def main():
+    print(passwordgen())
     return
 
 
